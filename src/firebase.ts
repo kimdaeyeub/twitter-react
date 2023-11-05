3a2470f32e36569d9a12ade67fa6c5ba3f91721d
kimdaeyeub <kdy3915@naver.com>
2023-11-06 00:54:29 +0900

    Setting

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJFrLGTuwHKI2O-JZn1l_mbEaJ5to42GA",
+  authDomain: "twitter-react-8dfc4.firebaseapp.com",
+  projectId: "twitter-react-8dfc4",
+  storageBucket: "twitter-react-8dfc4.appspot.com",
+  messagingSenderId: "467172162949",
+  appId: "1:467172162949:web:52a579a93a69ac03d958ec",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
