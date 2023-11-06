e4c541660f0d5ed7a636d7f3dbf296414df6b8ee
kimdaeyeub <kdy3915@naver.com>
2023-11-06 23:34:48 +0900

    Update

--- src/routes/CreateAccount.tsx
+++ src/routes/CreateAccount.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
 
 const CreateAccount = () => {
-  return <h1>CreateAccount</h1>;
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  return (
+    <Wrapper>
+      <Form>
+        <Input
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required
+        />
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value="Create Account" />
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default CreateAccount;
