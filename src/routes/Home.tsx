046368a6481f892a0027ecf016e3924e3372f952
kimdaeyeub <kdy3915@naver.com>
2023-11-10 00:16:33 +0900

    Fetching Timeline

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/PostTweetForm";
+import Timeline from "../components/Timeline";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-template-rows: 1fr 5fr;
+  scrollbar-width: thin;
+`;
 
 const Home = () => {
   return (
     <Wrapper>
       <PostTweetForm />
+      <Timeline />
     </Wrapper>
   );
 };
